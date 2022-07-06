976fef1f26e961c7940e94404d8acf66691d2085
Soyoung <56706291+Soyoung24@users.noreply.github.com>
2022-07-07 02:19:44 +0900

    Update asr_api.py

--- asr_api.py
+++ asr_api.py
@@ -35,4 +35,5 @@ result = streamlit_bokeh_events(
 
 if result:
     if "GET_TEXT" in result:
-        st.write(result.get("GET_TEXT"))
+        question=result.get("GET_TEXT")
+        st.write(question)
